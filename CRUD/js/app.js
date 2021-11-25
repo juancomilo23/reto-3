d1cb6b9f982242287e975bd01d3c8cd0c48c8b5a
juan camilo ospina chacon <88067572+juancomilo23@users.noreply.github.com>
2021-11-24 21:21:07 -0500

    Add files via upload

--- CRUD/js/app.js
+++ CRUD/js/app.js
@@ -1,18 +1,19 @@
-const firebaseConfig = {
+/*const firebaseConfig = {
     apiKey: "AIzaSyDR8foOYTe2cfTkEbkzKJTAFN19kwmzOPQ",
     authDomain: "crud-ed89d.firebaseapp.com",
     projectId: "crud-ed89d",
     storageBucket: "crud-ed89d.appspot.com",
     messagingSenderId: "1078567914017",
     appId: "1:1078567914017:web:977e5155964dc5b11ab160"
-}
+}*/
 
 const openModal = document .getElementById('openRegisterModal')
 const modal = document .getElementById('modal')
 const closeModal = document .getElementById('closeRegisterModal')
 const registerForm = document .getElementById('register-form')
-const app = initializeApp(firebaseConfig);
-const studentRef = firebase.database().ref('students')
+/*const registerForm = document .getElementById('btnSubmit')*/
+/*const app = initializeApp(firebaseConfig);
+const studentRef = firebase .getElementById ('students')*/
 
 const showRegisterModal = () => {
     modal.classList.toggle('is-active')
@@ -21,17 +22,27 @@ const showRegisterModal = () => {
 openModal .addEventListener('click', showRegisterModal)
 closeModal .addEventListener('click', showRegisterModal)
 
-registerForm.addEventListener('submit',(e) =>{
+registerForm.addEventListener('click',(e) =>{
     e.preventDefault()
 
     const nombre = registerForm['nombre'].value
     const apellido = registerForm['ape'].value
+    const matricula = registerForm['idm'].value
+    const estudiante = registerForm['ide'].value
+    const clase = registerForm['idc'].value
+    const titulo = registerForm['tit'].value
+    const descripcion = registerForm['des'].value
 
-    const registerStudent = studentRef.push()
+    const registerStudent = studentRef.submit()
     registerStudent.set({
         Uid: registerStudent.key,
         Nombre: nombre,
-        Apellido: apellido
+        Apellido: apellido,
+        IDmatricula: matricula,
+        IDestudiante: estudiante,
+        IDclase: clase,
+        Titulo: titulo,
+        Descripcion: descripcion
     })
     showRegisterModal()
 })
